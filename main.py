90cd9653a2d2f19869fc8934af231395cdd757ac
Stiffisharc <StiffisharcGitHub@pm.me>
2025-05-27 21:42:54 -0700

    Currently not functional. Pushing to ask for help adding a menu.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 import pygame
+import pygame_menu
 from constants import *
 from player import Player, Shot
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from startmenu import StartMenu
 
 def main():
     print("Starting Asteroids!")
@@ -11,8 +13,12 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    bg = pygame.image.load("Images/galaxy_background.jpg")
     clock = pygame.time.Clock()
     dt = 0
+
+    game_running = False
+    player = None
 
     updatables  = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
@@ -21,32 +27,60 @@ def main():
     
     Asteroid.containers = (updatables, drawables, asteroids)
     AsteroidField.containers = (updatables)
-    Player.containers = (updatables, drawables)
     Shot.containers = (updatables, drawables, shots)
     
     asteroidfield = AsteroidField()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        pygame.Surface.fill(screen, (0, 0, 0))
-        clock.tick(60)
-        updatables.update(dt)
+
+        if game_running == False:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+            screen.blit(bg, (0, 0))
+            clock.tick(60)
+            updatables.update(dt)
+
+            menu = StartMenu("Asteroids!", SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, theme=pygame_menu.themes.THEME_DARK)
+            menu.mainloop(screen)
+
+            for asteroid in asteroids:
+                asteroid.draw(screen)
+
+            dt = clock.get_time() / 1000
+            pygame.display.flip()
+
+        elif game_running == True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+            screen.blit(bg, (0, 0))
+            clock.tick(60)
+            updatables.update(dt)
+
+            if game_running == True:
+                Player.containers = (updatables, drawables)
+                if player == None:
+                    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         
-        for drawable in drawables:
-            drawable.draw(screen)
-        for asteroid in asteroids:
-            for shot in shots:
-                if asteroid.collision(shot) == True:
-                    asteroid.split()
-                    shot.kill()
-            if player.collision(asteroid) == True:
-                sys.exit("Game Over!")
+            for drawable in drawables:
+                drawable.draw(screen)
+            for asteroid in asteroids:
+                for shot in shots:
+                    if asteroid.collision(shot) == True:
+                        asteroid.split()
+                        shot.kill()
+                if player.collision(asteroid) == True:
+                    #sys.exit("Game Over!")
+                    game_running = False
         
-        dt = clock.get_time() / 1000
-        pygame.display.flip()
+            dt = clock.get_time() / 1000
+            pygame.display.flip()
+
+
+
+    pygame.quit()
+    sys.exit()
 
 if __name__ == "__main__":
     main()
---
+++ startmenu.py
@@ -0,0 +1,18 @@
+import sys
+import pygame
+import pygame_menu
+from pygame_menu import themes
+from asteroid import Asteroid
+
+class StartMenu(pygame_menu.Menu):
+    def __init__(self, title, width, height, theme):
+        super().__init__(title, width, height, True, None, 0, 1, True, True, True, theme)
+        self.add.button("Play", start())
+        self.add.button("Quit", pygame_menu.events.EXIT())
+        #pygame_menu kept giving me errors for various undefined variables so I manually entered them here. Any suggestions for why that was happening would be appreciated.
+
+    def start():
+        game_running = True
+        for asteroid in asteroids:
+            asteroid.kill()
+        return game_running
